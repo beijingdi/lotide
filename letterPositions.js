6e38ba682e48f2eec3de3d185a59c24b7f1c3ea1
Di <dwu233@gmail.com>
2022-04-06 20:24:06 +0000

    add

---
+++ letterPositions.js
@@ -0,0 +1,12 @@
+const letterPositions = function(str) {
+
+  let letterIndex = new Object;
+  str.split("").map(letter => letterIndex[letter] = []);
+  for (let i = 0; i < str.length; i++) {
+    letterIndex[str[i]].push(i);
+  }
+  return letterIndex;
+};
+
+console.log(letterPositions("Woody"));
+console.log(letterPositions("Woody")["o"]);
